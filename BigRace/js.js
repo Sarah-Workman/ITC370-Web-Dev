b4bf13ce2848ad506d15a51bb30246403e1fa98f
Sarah-Workman <55566625+Sarah-Workman@users.noreply.github.com>
2021-12-07 09:57:49 -0600

    v7 final verson

--- BigRace/js.js
+++ BigRace/js.js
@@ -37,11 +37,6 @@ document.getElementById('stopImage').addEventListener("dblclick", function(){
    
 
 
-function restartRace(){
-    document.getElementById("daenerys_TargaryenVid").style.visibility = "hidden"
-    document.getElementById("daenerys_TargaryenVid").style.zIndex = "2"
-
-}
 
 
 function myMove(){
@@ -52,7 +47,7 @@ const player1 = document.getElementById("john_Snow");
 const player2 = document.getElementById("daenerys_Targaryen");
 const player3 = document.getElementById("arya_Stark");
 
-//const players =[player1,player2, player3]
+
 let j = 0;
 let d = 0;
 let a = 0;
@@ -86,28 +81,40 @@ id = setInterval(animate, 10)
         clearInterval(id);
         document.getElementById("john_Snow").style.height = "500px"
         document.getElementById("john_Snow").style.width = "500px"
-        document.getElementById("john_Snow").style.marginLeft = "10%"
-        document.getElementById("john_Snow").style.marginTop = "10%"
+        document.getElementById("john_Snow").style.marginLeft = "-30em"
+        document.getElementById("john_Snow").style.marginTop = "3em"
         document.getElementById("p1").style.zIndex = "6"
+        document.getElementById("p1").style.visibility = "visible"
         document.getElementById("p1").innerHTML = "John Snow Is The Winner!!"
+        document.getElementById("john_Snow").addEventListener("dblclick", function(){
         restartRace();
+        })
     }else if(d >= 900 ){
       clearInterval(id);
-        document.getElementById("daenerys_TargaryenVid").style.visibility = " visible"
-        document.getElementById("daenerys_TargaryenVid").style.zIndex = "5"
-        document.getElementById("daenerys_TargarenVid").style.minHeight = "300%"
-        document.getElementById("daenerys_TargarenVid").style.minWeight = "120%"
-        document.getElementById("daenerys_TargarenVid").autoplay;
+      document.getElementById("daenerys_Targaryen").style.height = "500px"
+      document.getElementById("daenerys_Targaryen").style.width = "500px"
+      document.getElementById("daenerys_Targaryen").style.marginLeft = "-30em"
+      document.getElementById("daenerys_Targaryen").style.marginTop = "2.5em"
+      document.getElementById("p2").style.zIndex = "6"
+      document.getElementById("p2").style.visibility = "visible"
+      document.getElementById("p2").innerHTML = "Daenerys Targaryen Is The Winner!!"
+      document.getElementById("daenerys_Targaryen").addEventListener("dblclick", function(){
         restartRace();
+        })
+       
        
     }else if(a >= 900 ){
         clearInterval(id);
-        document.getElementById("daenerys_TargaryenVid").style.visibility = " visible"
-        document.getElementById("daenerys_TargaryenVid").style.zIndex = "5"
-        document.getElementById("daenerys_TargarenVid").style.minHeight = "300%"
-        document.getElementById("daenerys_TargarenVid").style.minWeight = "120%"
-        document.getElementById("daenerys_TargarenVid").autoplay;
-        restartRace();
+        document.getElementById("arya_Stark").style.height = "500px"
+        document.getElementById("arya_Stark").style.width = "500px"
+        document.getElementById("arya_Stark").style.marginLeft = "-30em"
+        document.getElementById("arya_Stark").style.marginTop = "2.5em"
+        document.getElementById("p3").style.zIndex = "6"
+        document.getElementById("p3").style.visibility = "visible"
+        document.getElementById("p3").innerHTML = "Arya Stark Is The Winner!!"
+        document.getElementById("arya_Stark").addEventListener("dblclick", function(){
+            restartRace();
+            })
     }
     
 
@@ -119,6 +126,53 @@ id = setInterval(animate, 10)
         
 }//myMove end
 
+function restartRace(){
+    //make the night king dissapear again 
+    document.getElementById("whiteWalker").style.visibility = "hidden"
+     
+    //hide paragraphs
+    document.getElementById("p1").style.visibility = "hidden"
+    document.getElementById("p2").style.visibility = "hidden"
+    document.getElementById("p3").style.visibility = "hidden"
+   
+    //move back to start
+    const player1 = document.getElementById("john_Snow");
+    const player2 = document.getElementById("daenerys_Targaryen");
+    const player3 = document.getElementById("arya_Stark");
+   
+    let j = 0;
+    let d = 0;
+    let a = 0;
+    
+    
+    player1.style.left = j + 'px';
+    player2.style.left = d + 'px';
+    player3.style.left = a + 'px';
+
+    //resize amd reset values
+    document.getElementById("john_Snow").style.height = "100px"
+        document.getElementById("john_Snow").style.width = "100px"
+        document.getElementById("john_Snow").style.marginLeft = "2em"
+        
+        document.getElementById("john_Snow").style.zIndex = "3"
+        
+
+        document.getElementById("daenerys_Targaryen").style.height = "100px"
+        document.getElementById("daenerys_Targaryen").style.width = "100px"
+        document.getElementById("daenerys_Targaryen").style.marginLeft = "2em"
+        document.getElementById("daenerys_Targaryen").style.marginTop = "10em;"
+        document.getElementById("daenerys_Targaryen").style.zIndex = "3"
+      
+
+        document.getElementById("arya_Stark").style.height = "100px"
+        document.getElementById("arya_Stark").style.width = "100px"
+        document.getElementById("arya_Stark").style.marginLeft = "2em"
+        document.getElementById("arya_Stark").style.marginTop = "20em"
+        document.getElementById("arya_Stark").style.zIndex = "3"
+        document.getElementById("p1").style.zIndex = "6"
+    //change the light back to red 
+    document.getElementById('stopImage').src='./images/redStopLight.jfif'
+}
 
 
 
